5eb3b88d3f6c784bfe2a9b0dd93ab05115f8a9cf
mfgobb <maxfahler@gmail.com>
2016-08-18 13:02:03 +0300

    add title, hero to app component

--- app/app.component.spec.ts
+++ app/app.component.spec.ts
@@ -18,28 +18,28 @@ describe('Smoke test', () => {
   });
 });
 
-describe('AppComponent with TCB', function () {
+describe('AppComponent with TCB', function() {
 
   it('should instantiate component',
     async(inject([TestComponentBuilder], (tcb: TestComponentBuilder) => {
 
-    tcb.createAsync(AppComponent).then(fixture => {
-      expect(fixture.componentInstance instanceof AppComponent).toBe(true, 'should create AppComponent');
-    });
-  })));
+      tcb.createAsync(AppComponent).then(fixture => {
+        expect(fixture.componentInstance instanceof AppComponent).toBe(true, 'should create AppComponent');
+      });
+    })));
 
   it('should have expected <h1> text',
     async(inject([TestComponentBuilder], (tcb: TestComponentBuilder) => {
 
       tcb.createAsync(AppComponent).then(fixture => {
-      // fixture.detectChanges();  // would need to resolve a binding but we don't have a binding
+        // fixture.detectChanges();  // would need to resolve a binding but we don't have a binding
 
-      let h1 = fixture.debugElement.query(el => el.name === 'h1').nativeElement;  // it works
+        let h1 = fixture.debugElement.query(el => el.name === 'h1').nativeElement;  // it works
 
-          h1 = fixture.debugElement.query(By.css('h1')).nativeElement;            // preferred
+        h1 = fixture.debugElement.query(By.css('h1')).nativeElement;            // preferred
 
-      expect(h1.innerText).toMatch(/angular 2 app/i, '<h1> should say something about "Angular 2 App"');
-    });
+        expect(h1.innerText).toMatch(/angular 2 app/i, '<h1> should say something about "Angular 2 App"');
+      });
 
-  })));
+    })));
 });
