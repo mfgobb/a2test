406c47495b23cf355e68cadf57df1071e9839d6e
mfgobb <maxfahler@gmail.com>
2016-08-19 17:32:36 +0300

    create hero-detail.component

---
+++ app/hero-detail.component.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by 220066 on 19/08/16.
+ */
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'my-hero-detail';
+  })
+
+export class HeroComponentDetail {
+
+}
